e99b9a3aa10d36687e62312b888dc2ead56c0040
dharana77 <dharana7723@gmail.com>
2021-08-13 21:56:12 +0900

    release Test

---
+++ hello_cargo/target/debug/deps/hello_cargo-bf49d0473f58984a.d
@@ -0,0 +1,5 @@
+/Users/jingyounglee/Desktop/projects/rust-in-action/hello_cargo/target/debug/deps/hello_cargo-bf49d0473f58984a.rmeta: src/main.rs
+
+/Users/jingyounglee/Desktop/projects/rust-in-action/hello_cargo/target/debug/deps/hello_cargo-bf49d0473f58984a.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/release/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/jingyounglee/Desktop/projects/rust-in-action/hello_cargo/target/release/hello_cargo: /Users/jingyounglee/Desktop/projects/rust-in-action/hello_cargo/src/main.rs
